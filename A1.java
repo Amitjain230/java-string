46de87bfd6b6c5b0cd186f8831fcded2f6d55802
Amitjain230 <amitassignment45@gmail.com>
2024-12-13 23:02:40 +0530

    success

---
+++ A1.java
@@ -0,0 +1,15 @@
+class A1{
+	public static void main(String args[]){
+	StringBuffer s=new StringBuffer("ABCDE");
+	
+	System.out.println(s);
+	
+	s.reverse();
+	System.out.println(s);
+
+
+
+
+
+  }
+}
