8d73db170e263b8370eb7129248f58769756eea3
Amitjain230 <amitassignment45@gmail.com>
2024-12-26 13:49:26 +0530

    success

---
+++ String33.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class S2{
+	public static void main(String args[]){
+	String str="Soft";
+	String str1="ware";
+	String str2=str+str1;
+	String str3="Software";
+	System.out.println(str2);
+	System.out.println(str3);
+	System.out.println(str2.compareTo(str3));
+	
+
+}
+}
+
+//charAt()method;;
